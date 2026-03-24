9f457348b2928b3fd15d21094ddbc26045eca045
Maria Torres <mafetorres622@gmail.com>
2026-03-24 13:20:25 -0500

    estructuraFinal

--- apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebcafeConfig(AppConfig):
-    name = 'webCafe'
--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.inicio),
-    path('persona/', views.persona),
-    path('propietario/', views.propietario),
-]
